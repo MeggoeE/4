f6406987ecd5c65dd99d431fc5cbdb3bad007e92
MeggoeE <117582790+MeggoeE@users.noreply.github.com>
2022-11-06 17:54:33 +0300

    Add files via upload

--- 2.4 reworked.py
+++ 2.4 reworked.py
@@ -1,13 +1,13 @@
-main_lenght = input()
+main_str = input()
 count = 0
 sum = 0
 lenght_of_sequence = 0
 while count < len(num):
-    if main_lenght[count] == '0':
+    if main_str[count] == '0':
         sum += 1
         if sum > lenght_of_sequence:
             lenght_of_sequence = sum
-    if main_lenght[count] == '1':
+    if main_str[count] == '1':
         sum = 0
     count += 1
 print(lenght_of_sequence)
